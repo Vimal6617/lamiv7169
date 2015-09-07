2711c1d5ce43254ea22355e0ff630b9ecbd51c3a
vimal <vimal@Vimals-MacBook.local>
2015-09-07 15:32:01 +0530

    Vimal - Add product
    Product Add Done

--- project/vkumar/Rahohealthy/src/main/java/com/rahohealthy/service/ProductdetailsService.java
+++ project/vkumar/Rahohealthy/src/main/java/com/rahohealthy/service/ProductdetailsService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.rahohealthy.model.Productdetails;
 
 public interface ProductdetailsService {
-	
+	public void addproduct(Productdetails productdetails);
 	public Productdetails getProductdetails(int product_id );
 	public List getAllProductdetails();
 }
